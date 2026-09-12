d296b36e650fb41eb16abfdd5fbe75ccd7e32b6f
Neha <nehasingh21032006@gmail.com>
2026-09-12 22:50:29 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./Components/Layout/Navbar";
+import Footer from "./Components/Layout/Footer";
+import heroImg from "./assets/hero.png";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Footer />
+    </>
+  )
+}
