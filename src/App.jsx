14c7174b0b07e8a9fd0598076c35dd23eade6b60
parasmandal146 <parasmandal146@gmail.com>
2026-09-13 15:10:05 +0530

    Added public Artist Profile view with custom artwork request modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import Navbar from "./Components/Layout/Navbar";
 import Footer from "./Components/Layout/Footer";
 import heroImg from "./assets/hero.png";
+import ArtistProfile from './pages/ArtistProfile';
+
+// Add this inside <Routes>:
+<Route path="/artist/:id" element={<ArtistProfile />} />
 
 function App() {
   return (
