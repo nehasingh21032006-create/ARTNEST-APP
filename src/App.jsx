d70f7966a639c0b050a5092d68ed4eba9634b220
Atharv Bhorkar <atharvbhorkar19@gmail.com>
2026-09-16 19:40:41 +0530

    Add Discover, Privacy, Terms pages and update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 import { Route } from 'react-router-dom'
 import Navbar from './Components/Navbar'
 import Home from './pages/Home' 
+import Discover from './pages/Discover' 
+import Privacy from './pages/Privacy'
+import Terms from './pages/Terms'
 import { Routes } from 'react-router-dom'
+
 
 const App = () => {
   return (
@@ -10,6 +14,9 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/discover" element={<Discover />} />
+        <Route path="/privacy" element={<Privacy />} />
+        <Route path="/terms" element={<Terms />} />
       </Routes>
       
     </div>
